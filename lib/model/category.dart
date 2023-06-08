8de4d684481c9fdcb58d4856eed73a55a0a95552
JahraSofieAzizah <133842893+JahraSofieAzizah@users.noreply.github.com>
2023-06-08 17:04:40 +0700

    Update category.dart

--- lib/model/category.dart
+++ lib/model/category.dart
@@ -7,14 +7,14 @@ class Category {
 }
 
 final homeCategries = <Category>[
-  const Category('assets/icons/category_sofa@2x.png', 'Hoodie', 'Hoodie'),
-  const Category('assets/icons/category_chair@2x.png', 'Kaos', 'Kaos'),
+  const Category('assets/icons/category_others@2x.png', 'Hoodie', 'Hoodie'),
+  const Category('assets/icons/category_others@2x.png', 'Kaos', 'Kaos'),
   const Category(
-      'assets/icons/category_table@2x.png', 'Crew Neck', 'Crew Neck'),
-  const Category('assets/icons/category_kitchen@2x.png', 'Dress', 'Dress'),
+      'assets/icons/category_others@2x.png', 'Crew Neck', 'Crew Neck'),
+  const Category('assets/icons/category_others@2x.png', 'Dress', 'Dress'),
   const Category(
-      'assets/icons/category_lamp@2x.png', 'Jam Tangan', 'Jam Tangan'),
-  const Category('assets/icons/category_cupboard@2x.png', 'Topi', 'Topi'),
-  const Category('assets/icons/category_vase@2x.png', 'Sepatu', 'Sepatu'),
+      'assets/icons/category_others@2x.png', 'Jam Tangan', 'Jam Tangan'),
+  const Category('assets/icons/category_others@2x.png', 'Topi', 'Topi'),
+  const Category('assets/icons/category_others@2x.png', 'Sepatu', 'Sepatu'),
   const Category('assets/icons/category_others@2x.png', 'Others', 'sofa'),
 ];
